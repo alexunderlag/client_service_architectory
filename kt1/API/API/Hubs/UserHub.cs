b95978cc833be4b4ed1adeee1542ce2b16f61f0c
alexunderlag <alexunderlag@ideal-web.site>
2024-12-28 10:13:22 +1000

    Add files via upload

---
+++ kt1/API/API/Hubs/UserHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using API.Models;
+using API.Data;
+
+namespace API.Hubs
+{
+    public class UserHub : Hub
+    {
+        private readonly UserRepository _repository;
+
+        public UserHub()
+        {
+            _repository = new UserRepository();
+        }
+
+        public async Task SendUserStatus(int userId, bool isOnline)
+        {
+            var user = _repository.GetById(userId);
+            if (user != null)
+            {
+                user.IsOnline = isOnline;
+                await Clients.All.SendAsync("ReceiveUserStatus", userId, isOnline);
+            }
+        }
+    }
+}
